e40d7315f2dec3da8f6a6fcaf5d1243a6638b5d2
egl-edu <egl-edu@proton.me>
2025-09-16 15:37:08 -0400

    Blocs statiques et plateforme mobile

--- projet_godot/scripts/controle_camera.gd
+++ projet_godot/scripts/controle_camera.gd
@@ -4,6 +4,7 @@ class_name ControleCamera extends Camera2D
 @export var zoom_closed := 1.3
 @export var zoom_open := 1.0
 @export var zoom_duration := 0.4
+
 var zoom_tween
 
 
@@ -17,7 +18,7 @@ func _ready() -> void:
 func zoom_in(_body):
 	if zoom_tween:
 		zoom_tween.kill()
-	zoom_tween = get_tree().create_tween()
+	zoom_tween = self.create_tween()
 	zoom_tween.set_trans(Tween.TRANS_CUBIC)
 	zoom_tween.tween_property(self, "zoom", Vector2.ONE * zoom_closed, zoom_duration)
 
@@ -25,6 +26,6 @@ func zoom_in(_body):
 func zoom_out(_body):
 	if zoom_tween:
 		zoom_tween.kill()
-	zoom_tween = get_tree().create_tween()
+	zoom_tween = self.create_tween()
 	zoom_tween.set_trans(Tween.TRANS_CUBIC)
 	zoom_tween.tween_property(self, "zoom", Vector2.ONE * zoom_open, zoom_duration)
